d61c91dc2641deb51858eab820ed6f69eb295f43
Benjamin Mimran <mimranbenjamin@gmail.com>
2017-02-06 15:56:16 +0100

    Stupid coach is definitely dumb now

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,23 @@
+class CoachingController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @query = params[:query]
+    @answer = coach_answer(@query)
+  end
+
+  private
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return "Ok bro"
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
